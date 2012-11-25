51225b700f5205c4df4fd858ac55d52dee99a2df
Reid Draper <reiddraper@gmail.com>
2012-11-25 13:41:51 -0500

    fuck macros

--- src/clj/todo/core.clj
+++ src/clj/todo/core.clj
@@ -1,11 +1,10 @@
 (ns todo.core
   (:use
-    [compojure.core :as compojure :only [ANY]]
-    [liberator.core :only [defresource]]))
+    [compojure.core :as compojure :only [ANY routes]]
+    [liberator.core :only [resource]]))
 
-(defresource home
-  :handle-ok "welcome home!"
-  :available-media-types ["text/plain"])
+(def home
+  (resource :handle-ok "welcome home!"
+            :available-media-types ["text/plain"]))
 
-(compojure.core/defroutes handler
-  (ANY "/" [] home))
+(def handler (routes (ANY "/" [] home)))
