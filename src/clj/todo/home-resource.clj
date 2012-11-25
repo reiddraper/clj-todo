9a3a2dcecb02168c6bc128a52dfbe83a80107090
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-25 17:14:43 -0500

    Add home resource.

---
+++ src/clj/todo/home-resource.clj
@@ -0,0 +1,18 @@
+(ns todo.home-resource
+  (:use [compojure.core :as compojure :only [ANY routes]])
+  (:require compojure.route
+            [liberator.core :as liberator]
+            [todo.formatters :as formatters]
+            [todo.model :as model]))
+
+(defn handle-ok
+  [req]
+  (let [list-ref (get-in req [:request :state])
+        lists (model/get-lists list-ref)
+        content-type (get-in req [:representation :media-type])
+        json-response (formatters/lists-json-serializer lists)]
+    (case content-type
+      "text/html" (formatters/generate-application-template json-response))))
+
+(def resource (liberator/resource :handle-ok handle-ok
+                                  :available-media-types ["text/html"]))
