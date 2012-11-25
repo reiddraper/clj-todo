9aede9a97ef28860b763b2560579942ca46b4442
Reid Draper <reiddraper@gmail.com>
2012-11-25 12:31:48 -0500

    add shafty git dep

--- project.clj
+++ project.clj
@@ -2,8 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-cljsbuild "0.2.7"]]
-  :source-paths ["src/clj" "src/cljs"]
+  :plugins [[lein-cljsbuild "0.2.7"]
+            [lein-git-deps "0.0.1-SNAPSHOT"]]
+  :git-dependencies  [["https://github.com/cmeiklejohn/shafty.git"]]
+  :source-paths ["src/clj" "src/cljs" ".lein-git-deps/shafty/src/cljs"]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds {:app {:source-path "src/cljs"
                               :compiler {:output-to "resources/public/application.js"
