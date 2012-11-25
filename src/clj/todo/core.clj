8607b5c045cdd4516c187a605f380df7a112a205
Reid Draper <reiddraper@gmail.com>
2012-11-25 14:17:55 -0500

    simple example of threading state through to the handler

--- src/clj/todo/core.clj
+++ src/clj/todo/core.clj
@@ -4,9 +4,14 @@
     [liberator.core :only [resource]])
   (:require compojure.route))
 
+(defn add-state [handler]
+  (fn [request]
+    (let [new-request (assoc request :state 5)]
+      (handler new-request))))
+
 (def home
-  (resource :handle-ok "welcome home!"
+  (resource :handle-ok (fn [req] (str "hello, the state is" (get-in req [:request :state])))
             :available-media-types ["text/plain"]))
 
-(def handler (routes (ANY "/" [] home)
+(def handler (routes (ANY "/" [] (add-state home))
                      (compojure.route/files "/"  {:root "resources/public"})))
