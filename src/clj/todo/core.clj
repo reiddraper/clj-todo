81b9037eced0d81c25ee1fdf5c2025c491be6b6f
Reid Draper <reiddraper@gmail.com>
2012-11-25 13:47:38 -0500

    add static file serving

--- src/clj/todo/core.clj
+++ src/clj/todo/core.clj
@@ -1,10 +1,12 @@
 (ns todo.core
   (:use
     [compojure.core :as compojure :only [ANY routes]]
-    [liberator.core :only [resource]]))
+    [liberator.core :only [resource]])
+  (:require compojure.route))
 
 (def home
   (resource :handle-ok "welcome home!"
             :available-media-types ["text/plain"]))
 
-(def handler (routes (ANY "/" [] home)))
+(def handler (routes (ANY "/" [] home)
+                     (compojure.route/files "/"  {:root "resources/public"})))
