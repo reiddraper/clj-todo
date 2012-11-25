7c4a14bb483aedeb236d7db4ddd1201338e8a4d3
Reid Draper <reiddraper@gmail.com>
2012-11-25 14:35:04 -0500

    create functions for modelling todo lists on server

---
+++ src/clj/todo/model.clj
@@ -0,0 +1,28 @@
+(ns todo.model)
+
+;; ---------------------------------------------------------------------------
+;; list management
+;; ---------------------------------------------------------------------------
+
+(defn create-list-state
+  "Create a new atom containing the state
+  for list items. Keys in the map are list
+  ids"
+  []
+  (atom {}))
+
+(defn get-list
+  "Get a list based on list id from list-ref.
+  If the list doesn't exist, return an empty vector"
+  [list-ref list-id]
+  (let [l (get @list-ref list-id)]
+    (if l @l
+      [])))
+
+(defn put-list!
+  "put new-list as the list for list-id in list-ref"
+  [list-ref list-id new-list]
+  (if (get @list-ref list-id)
+    (reset! (get @list-ref list-id)
+          new-list)
+    (swap! list-ref (fn [l] (assoc l list-id (atom new-list))))))
