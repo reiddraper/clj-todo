9292c640a13119c33c59edb5045636cf9fc0e8ef
Reid Draper <reiddraper@gmail.com>
2012-11-25 13:03:58 -0500

    hello world site working

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject todo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [liberator "0.6.0"]
+                 [compojure "1.1.3"]
+                 [hiccup "1.0.2"]]
   :plugins [[lein-cljsbuild "0.2.7"]
-            [lein-git-deps "0.0.1-SNAPSHOT"]]
+            [lein-git-deps "0.0.1-SNAPSHOT"]
+            [lein-ring "0.7.5"]]
+  :ring  {:handler todo.core/handler}
   :git-dependencies  [["https://github.com/cmeiklejohn/shafty.git"]]
   :source-paths ["src/clj" "src/cljs" ".lein-git-deps/shafty/src/cljs"]
   :hooks [leiningen.cljsbuild]
