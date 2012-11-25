f36164dae0a9fab554f897c5de1765754306f401
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-25 13:10:46 -0500

    Add base application file.

--- src/cljs/todo/application.cljs
+++ src/cljs/todo/application.cljs
@@ -1,2 +1,9 @@
 (ns todo.application
-  (:require [shafty.event :as event]))
+  (:require-macros [hiccups.core :as hiccups])
+  (:require [hiccups.runtime :as hiccupsrt]
+            [shafty.event :as event]
+            [shafty.behaviour :as behaviour]))
+
+(defn main []
+  "Start the application."
+  (.log js/console "Boo!"))
